2abd854942f7eb70d7a75584249effd4632ddd1c
roufurrohim <muhamadbarruroufurrohim@gmail.com>
2021-11-25 15:24:09 +0700

    updating port

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const http = require('http')
-const { Server } = require('socket.io')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
 
-const usersRouter = require('./src/routers/users')
+const usersRouter = require("./src/routers/users");
 
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
-app.use(usersRouter)
-app.use(express.static(__dirname + '/uploads'))
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(usersRouter);
+app.use(express.static(__dirname + "/uploads"));
 
-const httpServer = http.createServer(app)
+const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
-    cors: {
-        origin: "*",
-    }
-})
+  cors: {
+    origin: "*",
+  },
+});
 
-require('./src/socket/consumer')(io)
+require("./src/socket/consumer")(io);
 
-const port = 5000
-httpServer.listen(port, () => {
-    console.log(`Service Running on port ${port}`)
-})
+const { PORT = 3004, LOCAL_ADDRESS = "0.0.0.0" } = process.env;
+app.listen(PORT, LOCAL_ADDRESS, () => {
+  console.log(`Service running on port ${PORT}`);
+});
 
-module.exports = app
+module.exports = app;
